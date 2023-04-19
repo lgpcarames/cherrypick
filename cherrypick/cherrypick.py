15131656c9a5f154ee1b340289c3647d74f45b21
Lucas Caramês <lgpcarames@gmail.com>
2023-04-19 17:09:33 -0300

    add: classe CatEncoder

--- cherrypick/cherrypick.py
+++ cherrypick/cherrypick.py
@@ -1,1 +1,84 @@
-"""Main module."""
+from functools import reduce
+from typing import Union, Any
+import pandas as pd
+
+
+# Categorical features process
+class CatEncoder():
+    """
+    Similarly to SKLearn LabelEncoder, but it does work with new categories.
+
+    To be use:
+    ----------
+    ce = CatEncoder()
+    ce.fit(dataframe[selected_columns])
+
+    ce.transform(dataframe[selected_columns])
+    
+
+    Developed by: Vinícius Ormenesse - https://github.com/ormenesse
+    """
+
+    def __init__(self) -> None:
+        """
+        Construtor da classe
+        """
+        self.dic = {}
+        self.rev_dic = {}
+
+    def fit(self, vet: Union[pd.DataFrame, pd.Series, list]) -> None:
+        """
+        Prepara o encoder para os dados passados na variável vet.
+        """
+        uniques = []
+        for c in vet.unique():
+            if str(type(c)) == "<class 'str'>":
+                uniques.append(c)
+        uniques.sort()
+        for a, b in enumerate(uniques):
+            self.dic[b] = a
+            self.rev_dic[a] = b
+        return self
+    def check(
+            self,
+            vet: Union[pd.DataFrame, pd.Series, list]
+            ) -> Union[pd.DataFrame, pd.Series]:
+        """
+        Checa o tipo da variável vet. Transformando em pd.Series, caso seja do tipo list.
+        """
+        try:
+            if type(vet) == list:
+                return pd.Series(vet)
+            return vet
+        except Exception:
+            return vet
+    def transform(self, vet: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
+        """
+        Realiza a transformação do label de acordo com o que foi adaptado pelo encoder.
+        """
+        vet = self.check(vet)
+        return vet.map(self.dic)
+    
+    def inverse_transform(self, vet: Union[pd.DataFrame, pd.Series]) -> Union[pd.DataFrame, pd.Series]:
+        """
+        Inverte a transformação realizada pelo encoder.
+        """
+        vet = self.check(vet)
+        return vet.map(self.rev_dic)
+    
+    def fit_transform(self, vet: Union[pd.DataFrame, pd.Series]) -> Union[pd.DataFrame, pd.Series]:
+        """
+        Adapta o encoder aos dados e em seguida os transforma.
+        """
+        vet = self.check(vet)
+        self.fit(vet.astype(str))
+        return self.transform(vet.astype(str))
+    
+
+
+if __name__=='__main__':
+    vet_test = pd.Series(['Variavel', 'Nao-Variavel', 'Variavel', 'Nao-Variavel', 'Variavel', 'Nao-Variavel', 'Variavel', 'Nao-Variavel'])
+
+
+    ce = CatEncoder()
+    print(ce.fit_transform(vet_test))
