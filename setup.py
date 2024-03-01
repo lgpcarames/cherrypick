43256d362164da1d2caa320daa0906c8c7dcea03
Lucas Caramês <81190139+lgpcarames@users.noreply.github.com>
2024-02-29 21:01:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     description="Some tools to help the process of feature selection",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description_content_type=readme + '\n\n' + history,
     include_package_data=True,
     keywords='cherrypick',
     name='cherrypick',
